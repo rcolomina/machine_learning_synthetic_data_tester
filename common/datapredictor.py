754139020ab0f74cda38ada15f9955210d152c0a
Ruben Colomina <rcolomina@gmail.com>
2017-10-28 22:14:49 +0100

    Added generic methods. Use NotImplementedError method instead keyword pass.

--- common/datapredictor.py
+++ common/datapredictor.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python
-from interface import Interface
+#from interface import Interface
 
-class Predictor(Interface):
+class Predictor(object):
     # Return a prediction using a sample
     def predict(self,sample):
         pass
     # Train the model underneath the predictor
+    #def train(self,Xtrain,Ytrain):
+    #    pass
+
     def train(self,Xtrain,Ytrain):
-        pass
+        # fit the model with training data
+        self.model.fit(Xtrain,Ytrain)
+        self.samplestrained = len(Xtrain)
+        self.trained = True
+        print "Model trained"
+        return True
+
     # Test the model scoring on test data
     def score(self,Xtest,Ytest):
         pass
