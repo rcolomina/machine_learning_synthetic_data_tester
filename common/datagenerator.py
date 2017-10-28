1de648b90525a3270aee63924aacc43bae158a9f
Ruben Colomina <rcolomina@gmail.com>
2017-10-28 22:16:29 +0100

    Added generic methods. Use NotImplementedError method instead keyword pass.

--- common/datagenerator.py
+++ common/datagenerator.py
@@ -1,27 +1,52 @@
 #!/usr/bin/python
-from interface import Interface
 
-class DataGenerator(Interface):
-    
+class DataGenerator(object):
     def model(self):
-        pass
+        raise NotImplementedError()    
     def genSerie(self,seriesize):
-        pass
-    def getData(self):
-        pass
-    def printParams(self):
-        pass
-    def writeData(self):
-        pass
+        raise NotImplementedError()
+
+#    def printParams(self):
+#        pass
+#    def writeData(self):
+#        pass
     
     # common implementation
     def gen(self):
         register = self.model()
         self.counter += 1
         return register
+    
     def reset(self):
         counter = 0
         self.data = []
+        
+    def getData(self):
+        return self.data
+
+    def getTrainData(self):
+        if self.X == []:
+            return False
+        
+        X = self.X
+        Y = self.Y
+        
+        trainsize = int(len(X) * self.train_percent)
+        Xtrain = X[0:trainsize]
+        Ytrain = Y[0:trainsize]
+        return Xtrain,Ytrain        
+
+    def getTestData(self):
+        if self.X == []:
+            return False
+        X = self.X
+        Y = self.Y
+
+        trainsize = int(len(X) * self.train_percent)
+        Xtest = X[trainsize:len(X)]
+        Ytest = Y[trainsize:len(X)]
+
+        return Xtest,Ytest
 
     
 
