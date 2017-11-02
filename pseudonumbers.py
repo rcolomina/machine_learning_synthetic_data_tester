dd748db7657584d01d8e49812b70a71e964f487e
Ruben Colomina <rcolomina@gmail.com>
2017-11-02 19:19:51 +0000

    Removed generic functions.

--- pseudonumbers.py
+++ pseudonumbers.py
@@ -10,7 +10,7 @@ class PseudoNumbers(DataGenerator):
                  sizeX=20,foreY=5,
                  train_percent=0.7,
                  seriesize=10000):
-        
+
         self.data = []
         self.noise  = noise
         self.counter= 0
@@ -38,41 +38,11 @@ class PseudoNumbers(DataGenerator):
 
         randomsample = random.random() -  0.5
         return self.calmodel + self.counter + randomsample * self.noise
-        
-#    def gen(self):
-#        register = self.model()
-#        self.counter += 1
-#        return register
-
-#    def reset(self):
-#        self.counter = 0
-#        self.data = []
-    
-    def genSerie(self,seriesize):
-        serie = []
-        for i in range(0,seriesize):
-            newData = self.gen()
-            self.data.append(newData)
-            serie.append(newData)
-        
-        # Create X,Y arrays
-        self.X,self.Y = self.getXYData()
-    
+                
     def printParams(self):
         print "Noise:",self.noise
         print "Counter:",self.counter
-        
-    def getXYData(self):
-        xDim = self.sizeX
-        yfd  = self.foreY
-        if xDim < 1 or yfd < 1:
-            return False
-        imax = len(self.data)-xDim-yfd+1        
-       #print len(self.data)
-        X = [self.data[i:i+xDim] for i in xrange(0,imax)]
-        Y = [self.data[i+xDim+yfd-1] for i in range(0,imax)]
-        return X,Y
-                    
+                            
     def writeData(self):
         print "TODO"
 
