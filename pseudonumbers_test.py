ba059587b44924960d803849f898025fe4309034
Ruben Colomina <rcolomina@gmail.com>
2018-04-29 16:22:38 +0100

    Cosmetic changes

--- pseudonumbers_test.py
+++ pseudonumbers_test.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
-
 from pseudonumbers import PseudoNumbers
 import matplotlib.pyplot as plt
 
 # Generate synthetic data
 mynoise = PseudoNumbers()
-mynoise.genSerie(10000)
+mynoise.genSerie(seriesize=10000)
+
 Xtrain,Ytrain = mynoise.getTrainData()
 Xtest,Ytest   = mynoise.getTestData()
 
 # Plot synthetic data
 gendata = mynoise.getData()
+
 #plt.plot(gendata)
 #plt.ylabel('PseudoNumbers')
 #plt.show()
-
 print "Test Executed Successfully"
-
+print "First terms:",gendata[0:10]
 
