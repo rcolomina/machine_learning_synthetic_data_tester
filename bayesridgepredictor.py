040cd74ecab921dac9e79c0c5a1a0e063a2b70f1
Ruben Colomina <rcolomina@gmail.com>
2018-04-29 16:19:38 +0100

    Checking that model is not none

--- bayesridgepredictor.py
+++ bayesridgepredictor.py
@@ -6,7 +6,9 @@ from common.datapredictor import Predictor
 
 class BayesRidgePredict(Predictor):    
     def __init__(self):
+        # Set us not trained
         self.trained = False
+        # Set the model to bayesian ridge
         self.model = linear_model.BayesianRidge()
         self.samplestrained = 0
         
@@ -18,14 +20,15 @@ class BayesRidgePredict(Predictor):
             ret_model = self.model.predict(sample)
             return ret_model
         except ValueError:
-            print "Model could not predict ",ValueError
-    
+            print "Model could not predict ",ValueError    
         
     # Get score on test data
     def score(self,Xtest,Ytest):
+        if self.model != None:
         # check (Xtest, Ytest) are different from (Xtrain,Ytrain)
-        return self.model.score(Xtest,Ytest)
-
+            return self.model.score(Xtest,Ytest)
+        return None
+        
     #Partialy train
     def update(self,Xup,Yup):
         self.trained = True
