8611779f923b12b476a3adb7bef8aa5913066685
Ruben Colomina <rcolomina@gmail.com>
2018-04-29 16:21:23 +0100

    check that model is not none

--- common/datapredictor.py
+++ common/datapredictor.py
@@ -2,25 +2,24 @@
 #from interface import Interface
 
 class Predictor(object):
+
+    def __init__(self):
+        self.model = None
+        self.samplestrained = 0
+        self.trained = False
+            
     # Return a prediction using a sample
     def predict(self,sample):
         pass
+    
     # Train the model underneath the predictor
-    #def train(self,Xtrain,Ytrain):
-    #    pass
-
     def train(self,Xtrain,Ytrain):
-        # fit the model with training data
-        self.model.fit(Xtrain,Ytrain)
-        self.samplestrained = len(Xtrain)
-        self.trained = True
-        print "Model trained"
-        return True
-
-    # Test the model scoring on test data
-    def score(self,Xtest,Ytest):
-        pass
-    # Update the model with new data
-#    def update(self,X,Y):
-#        pass
+        if self.model != None:
+            # fit the model with training data
+            self.model.fit(Xtrain,Ytrain)
+            self.samplestrained = len(Xtrain)
+            self.trained = True
+            print "Model trained"
+            return True
+        return False
         
