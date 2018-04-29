9a9a1a6c12d29004952b616a84084e4655948aad
Ruben Colomina <rcolomina@gmail.com>
2018-04-29 16:22:14 +0100

    Removed print outs.

--- gensinnoise_test.py
+++ gensinnoise_test.py
@@ -2,11 +2,12 @@
 from gensinnoise import SinNoise
 
 mynoise = SinNoise(10.0,0.5,0.0,0.1)
-mynoise.genSerie(50)
-X,Y = mynoise.getXYData(2,4)
 
-print "DATA:",mynoise.getData()
-print "X",X
+mynoise.genSerie(30)
+X,Y = mynoise.getXYData()
+
+#print "DATA:",mynoise.getData()
+#print "X",X
 print "len(X):",len(X)
-print "Y",Y
+#print "Y",Y
 print "Y:",len(Y)
